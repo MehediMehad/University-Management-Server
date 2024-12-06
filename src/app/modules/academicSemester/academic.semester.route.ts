6c8d4fbf9e5f824abaa4f74d79633e709512f15e
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-12-06 11:18:56 +0600

    Update Academic Semester

--- src/app/modules/academicSemester/academic.semester.route.ts
+++ src/app/modules/academicSemester/academic.semester.route.ts
@@ -1,20 +1,30 @@
 import express from 'express';
 import { AcademicSemesterControllers } from './academic.semester.controller';
 import validateRequest from '../../Middleware/validateRequest';
-import { AcademicSemesterValidation } from './academic.semester.validation';
+import { AcademicSemesterValidations } from './academic.semester.validation';
 const router = express.Router();
 
 router.post(
     '/create-academic-semester',
     validateRequest(
-        AcademicSemesterValidation.createAcademicSemesterValidationSchema
+        AcademicSemesterValidations.createAcademicSemesterValidationSchema
     ),
     AcademicSemesterControllers.createAcademicSemester
 );
+
 router.get(
     '/:semesterId',
     AcademicSemesterControllers.getSingleAcademicSemester
 );
+
+router.patch(
+    '/:semesterId',
+    validateRequest(
+        AcademicSemesterValidations.updateAcademicSemesterValidationSchema
+    ),
+    AcademicSemesterControllers.updateAcademicSemester
+);
+
 router.get('/', AcademicSemesterControllers.getAllAcademicSemesters);
 
 export const AcademicSemesterRouters = router;
