8e4a74e1d269656076e63dcc53ad14423b8d82da
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-12-23 01:09:28 +0600

    check if the academic semester is already registered and semester registration

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 import AppError from '../../errors/AppError';
 import { AcademicSemester } from '../academicSemester/academicSemester.model';
 import { TSemesterRegistration } from './semesterRegistration.interface';
+import { SemesterRegistration } from './semesterRegistration.model';
 
 const createSemesterRegistrationIntoDB = async (
     payload: TSemesterRegistration
@@ -19,6 +20,18 @@ const createSemesterRegistrationIntoDB = async (
         );
     }
     //check if the academic semester is already registered
+    const isSemesterRegistrationExist = await SemesterRegistration.findOne({
+        academicSemester
+    });
+    if (isSemesterRegistrationExist) {
+        throw new AppError(
+            StatusCodes.CONFLICT,
+            'This academic semester is already registered'
+        );
+    }
+    // create semester registration
+    const result = await SemesterRegistration.create(payload);
+    return result;
 };
 const getAllSemesterRegistrationsFromDB = async () => {};
 const getSingleSemesterRegistrationsFromDB = async () => {};
