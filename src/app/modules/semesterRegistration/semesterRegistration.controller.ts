8eea6551c453f836d6d17a24fa323fb72425e212
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-12-23 00:44:50 +0600

    check if the academic semester exists

--- src/app/modules/semesterRegistration/semesterRegistration.controller.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.controller.ts
@@ -1,14 +1,68 @@
+import { Request, Response } from 'express';
 import catchAsync from '../../utils/catchAsync';
+import { SemesterRegistrationService } from './semesterRegistration.service';
+import sendResponse from '../../utils/sendResponse';
+import { StatusCodes } from 'http-status-codes';
 
-const createSemesterRegistration = catchAsync();
+const createSemesterRegistration = catchAsync(
+    async (req: Request, res: Response) => {
+        const result =
+            await SemesterRegistrationService.createSemesterRegistrationIntoDB(
+                req.body
+            );
 
-const getAllSemesterRegistrations = catchAsync();
+        sendResponse(res, {
+            statusCode: StatusCodes.OK,
+            success: true,
+            message: 'Semester Registration is created successfully!',
+            data: result
+        });
+    }
+);
 
-const getSingleSemesterRegistration = catchAsync();
+const getAllSemesterRegistrations = catchAsync(
+    async (req: Request, res: Response) => {
+        // sendResponse(res, {
+        //   statusCode: StatusCodes.OK,
+        //   success: true,
+        //   message: '',
+        //   data: result,
+        // });
+    }
+);
 
-const updateSemesterRegistration = catchAsync();
+const getSingleSemesterRegistration = catchAsync(
+    async (req: Request, res: Response) => {
+        // sendResponse(res, {
+        //   statusCode: StatusCodes.OK,
+        //   success: true,
+        //   message: '',
+        //   data: result,
+        // });
+    }
+);
 
-const deleteSemesterRegistration = catchAsync();
+const updateSemesterRegistration = catchAsync(
+    async (req: Request, res: Response) => {
+        // sendResponse(res, {
+        //   statusCode: StatusCodes.OK,
+        //   success: true,
+        //   message: '',
+        //   data: result,
+        // });
+    }
+);
+
+const deleteSemesterRegistration = catchAsync(
+    async (req: Request, res: Response) => {
+        // sendResponse(res, {
+        //   statusCode: StatusCodes.OK,
+        //   success: true,
+        //   message: '',
+        //   data: result,
+        // });
+    }
+);
 
 export const SemesterRegistrationController = {
     createSemesterRegistration,
--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,4 +1,25 @@
-const createSemesterRegistrationIntoDB = async () => {};
+import { StatusCodes } from 'http-status-codes';
+import AppError from '../../errors/AppError';
+import { AcademicSemester } from '../academicSemester/academicSemester.model';
+import { TSemesterRegistration } from './semesterRegistration.interface';
+
+const createSemesterRegistrationIntoDB = async (
+    payload: TSemesterRegistration
+) => {
+    const academicSemester = payload?.academicSemester;
+
+    //check if the academic semester exists
+    const isAcademicSemesterExist =
+        await AcademicSemester.findById(academicSemester);
+
+    if (!isAcademicSemesterExist) {
+        throw new AppError(
+            StatusCodes.NOT_FOUND,
+            'Academic semester not found'
+        );
+    }
+    //check if the academic semester is already registered
+};
 const getAllSemesterRegistrationsFromDB = async () => {};
 const getSingleSemesterRegistrationsFromDB = async () => {};
 const updateSemesterRegistrationIntoDB = async () => {};
