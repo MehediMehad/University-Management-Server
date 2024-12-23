4c02942d82b8244226d5b60de67a55221ebdf0f8
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-12-23 17:52:34 +0600

    create OfferedCourseValidationSchema

---
+++ src/app/modules/OfferedCourse/OfferedCourse.validation.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+import { Days } from './OfferedCourse.constant';
+
+const createOfferedCourseValidationSchema = z.object({
+    body: z.object({
+        semesterRegistration: z.string(),
+        academicSemester: z.string(),
+        academicFaculty: z.string(),
+        academicDepartment: z.string(),
+        course: z.string(),
+        faculty: z.string(),
+        section: z.number(),
+        maxCapacity: z.number(),
+        days: z.array(z.enum([...Days] as [string, ...string[]])),
+        startTime: z.string(),
+        endTime: z.string()
+    })
+});
+
+export const OfferedCourseValidations = {
+    createOfferedCourseValidationSchema
+};
