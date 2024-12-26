8af1f4bab1769eb8829915fb8102a1b0d5589350
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-12-26 11:21:40 +0600

    Create Auth interface , controller ,validation service

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { StatusCodes } from 'http-status-codes';
 import AppError from '../../errors/AppError';
