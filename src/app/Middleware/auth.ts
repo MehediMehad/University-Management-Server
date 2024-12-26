a7464ca6b388eb3f85ede40b926a38d1ac7fa3ee
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-12-26 21:34:34 +0600

    authorize using auth

--- src/app/Middleware/auth.ts
+++ src/app/Middleware/auth.ts
@@ -4,11 +4,12 @@ import AppError from '../errors/AppError';
 import { StatusCodes } from 'http-status-codes';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
+import { TUserRole } from '../modules/user/user.interface';
 
 export interface CustomRequest extends Request {
     user: JwtPayload;
 }
-const auth = () => {
+const auth = (...requiredRoles: TUserRole[]) => {
     return catchAsync(
         async (req: Request, res: Response, next: NextFunction) => {
             const token = req.headers.authorization;
@@ -25,6 +26,13 @@ const auth = () => {
                 config.jwt_access_secret as string,
                 function (err, decoded) {
                     if (err) {
+                        throw new AppError(
+                            StatusCodes.UNAUTHORIZED,
+                            'You are not authorized!'
+                        );
+                    }
+                    const role = (decoded as JwtPayload).role;
+                    if (requiredRoles && !requiredRoles.includes(role)) {
                         throw new AppError(
                             StatusCodes.UNAUTHORIZED,
                             'You are not authorized!'
