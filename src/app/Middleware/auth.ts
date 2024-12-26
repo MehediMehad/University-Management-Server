035d843e591cbe2388a6380a173d0e27736b6584
MehediMehad <132126469+MehediMehad@users.noreply.github.com>
2024-12-26 21:05:37 +0600

    declare global ব্যবহার করা হয়েছে, যাতে আমরা TypeScript-এর গ্লোবাল স্কোপে নতুন টাইপ বা ইন্টারফেস যোগ করতে পারি।

--- src/app/Middleware/auth.ts
+++ src/app/Middleware/auth.ts
@@ -2,20 +2,38 @@ import { Request, Response, NextFunction } from 'express';
 import catchAsync from '../utils/catchAsync';
 import AppError from '../errors/AppError';
 import { StatusCodes } from 'http-status-codes';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
 
+export interface CustomRequest extends Request {
+    user: JwtPayload;
+}
 const auth = () => {
     return catchAsync(
         async (req: Request, res: Response, next: NextFunction) => {
             const token = req.headers.authorization;
-
+            // if the token is sent from the client
             if (!token) {
                 throw new AppError(
                     StatusCodes.UNAUTHORIZED,
                     'You are not authorized!'
                 );
             }
-
-            next();
+            // check if the token is valid
+            jwt.verify(
+                token,
+                config.jwt_access_secret as string,
+                function (err, decoded) {
+                    if (err) {
+                        throw new AppError(
+                            StatusCodes.UNAUTHORIZED,
+                            'You are not authorized!'
+                        );
+                    }
+                    req.user = decoded as JwtPayload;
+                    next();
+                }
+            );
         }
     );
 };
